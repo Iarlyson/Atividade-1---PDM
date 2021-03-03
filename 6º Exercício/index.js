a83447c744eb428244f8a4e3f812d9efe99b42cd
Iarlyson Santana <Iarlyson.Santana@gmail.com>
2021-03-03 19:18:18 -0300

    Exercicio 6 feito

---
+++ 6º Exercício/index.js
@@ -0,0 +1,14 @@
+const usuario = 'Iarlyson';
+const idade = 23;
+console.log(usuario)
+const mensagem = `o usuario ${usuario}
+possui ${idade} anos`;
+
+function printUser (): string {
+    const usuario = 'Iarlyson';
+    const idade = 23;
+    return  `o usuario ${usuario}
+    possui ${idade} anos`; 
+}
+
+console.log(printUser)
