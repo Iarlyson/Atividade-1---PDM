3d0413955ac32cb56a1ecb875a2463d9c62c95e5
daniel-dantas <daniel.dantas.ip19@hotmail.com>
2021-03-05 00:06:40 -0300

    concluindo questão 05

--- 5º Exercicio/index.js
+++ 5º Exercicio/index.js
@@ -18,3 +18,21 @@ const y = [...arr];
 
 console.log(x);
 console.log(y);
+
+// 5.2
+
+const usuario = {
+  nome: "Diego",
+  idade: 23,
+  endereco: {
+    cidade: "Rio do Sul",
+    uf: "SC",
+    pais: "Brasil",
+  },
+};
+
+const usuario2 = { ...usuario, nome: "Gabriel" };
+
+const usuario3 = { ...usuario, nome: "Lontras" };
+
+console.log(usuario2, usuario3);
